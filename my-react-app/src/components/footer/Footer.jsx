cd8b3b986281b9c8aff186e9d80d58dfc9fe6cc6
Jorge Sandoval <jorge9005@outlook.com>
2023-07-01 10:31:43 -0600

    Modificación en el footer

--- my-react-app/src/components/footer/Footer.jsx
+++ my-react-app/src/components/footer/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
         <footer>
             <div className="social"><a href="#"><i className="icon ion-social-instagram" /></a><a href="#"><i className="icon ion-social-snapchat" /></a><a href="#"><i className="icon ion-social-twitter" /></a><a href="#"><i className="icon ion-social-facebook" /></a></div>
             <ul className="list-inline">
-                <li className="list-inline-item"><a href="#">Home</a></li>
-                <li className="list-inline-item"><a href="#">Services</a></li>
-                <li className="list-inline-item"><a href="#">About</a></li>
-                <li className="list-inline-item"><a href="#">Terms</a></li>
-                <li className="list-inline-item"><a href="#">Privacy Policy</a></li>
+                <li className="list-inline-item"><a href="/">Home</a></li>
+                <li className="list-inline-item"><a href="/">Services</a></li>
+                <li className="list-inline-item"><a href="/">About</a></li>
+                <li className="list-inline-item"><a href="/">Terms</a></li>
+                <li className="list-inline-item"><a href="/">Privacy Policy</a></li>
             </ul>
             <p className="copyright">Company Name © 2018</p>
         </footer>
