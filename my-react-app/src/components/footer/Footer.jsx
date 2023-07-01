802aac3971c12655852055d91683af9cbad817fe
Jorge Sandoval <jorge9005@outlook.com>
2023-07-01 17:31:17 -0600

    Correción de error para build

--- my-react-app/src/components/footer/Footer.jsx
+++ my-react-app/src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" />
         <footer>
-            <div className="social"><a href="www.google.com"><i className="icon ion-social-instagram" /></a><a href="www.google.com"><i className="icon ion-social-snapchat" /></a><a href="www.google.com"><i className="icon ion-social-twitter" /></a><a href="#"><i className="icon ion-social-facebook" /></a></div>
+            <div className="social"><a href="www.google.com"><i className="icon ion-social-instagram" /></a><a href="www.google.com"><i className="icon ion-social-snapchat" /></a><a href="www.google.com"><i className="icon ion-social-twitter" /></a><a href="www.google.com"><i className="icon ion-social-facebook" /></a></div>
             <ul className="list-inline">
                 <li className="list-inline-item"><a href="www.google.com">Home</a></li>
                 <li className="list-inline-item"><a href="www.google.com">Services</a></li>
